a7cf0c801c823739838ca1bf79e1d251b4a9ea4f
vazheninEgor <m2310263@edu.misis.ru>
2024-12-03 13:52:14 +0300

    add lab2

---
+++ lab3/singleton.py
@@ -0,0 +1,17 @@
+class Singleton:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def some_method(self):
+        return "This is a Singleton method."
+
+# Пример использования
+singleton1 = Singleton()
+singleton2 = Singleton()
+
+print(singleton1 is singleton2)  # True
+print(singleton1.some_method())
